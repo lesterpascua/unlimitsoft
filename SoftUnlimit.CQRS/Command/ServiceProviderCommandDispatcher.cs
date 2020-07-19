0fcd15dd570f7068c2728d0b4d188eca8446334d
Lester Pastrana <lesterpascua@gmail.com>
2020-07-19 14:36:45 +0200

    amend compiler error

--- SoftUnlimit.CQRS/Command/ServiceProviderCommandDispatcher.cs
+++ SoftUnlimit.CQRS/Command/ServiceProviderCommandDispatcher.cs
@@ -29,7 +29,7 @@ namespace SoftUnlimit.CQRS.Command
         /// <summary>
         /// Default function used to convert error transform to standard ASP.NET format
         /// </summary>
-        public static readonly Func<IList<ValidationFailure>, IDictionary<string, IEnumerable<string>>> DefaultErrorTransforms = (failure) => failure.GroupBy(p => p.PropertyName).ToDictionary(k => k.Key, v => v.Select(s => s.ErrorMessage))
+        public static readonly Func<IList<ValidationFailure>, IDictionary<string, IEnumerable<string>>> DefaultErrorTransforms = (failure) => failure.GroupBy(p => p.PropertyName).ToDictionary(k => k.Key, v => v.Select(s => s.ErrorMessage));
 
 
         /// <summary>
