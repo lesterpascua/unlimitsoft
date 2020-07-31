d8c06b96d72a4d79a6ab4413ca52fa8fa4ff622d
Lester Pastrana <lesterpascua@gmail.com>
2020-07-31 21:13:38 +0200

    implement abstract class

--- SoftUnlimit.CQRS/Query/QueryAsync.cs
+++ SoftUnlimit.CQRS/Query/QueryAsync.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SoftUnlimit.CQRS.Query
 {
     /// <summary>
     /// Base class for all query.
     /// </summary>
-    public abstract class QueryAsync<T> : IQueryAsync
-        where T : QueryProps
+    public abstract class QueryAsync<TResult> : IQueryAsync<TResult>
+        where TResult : QueryProps
     {
         /// <summary>
-        /// Get or set metadata props associate with the command.
+        /// Get or set metadata props associate with the query.
         /// </summary>
-        public T QueryProps { get; protected set; }
+        public TResult QueryProps { get; protected set; }
+
+        /// <summary>
+        /// Auto execute query.
+        /// </summary>
+        /// <param name="dispatcher"></param>
+        /// <returns></returns>
+        public Task<TResult> ExecuteAsync(IQueryAsyncDispatcher dispatcher) => dispatcher.DispatchAsync<TResult>(this);
 
         /// <summary>
         /// Return metadata props associate with the query.
