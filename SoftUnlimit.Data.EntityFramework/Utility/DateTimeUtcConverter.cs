f0d0ba4df04275886816553695cc790c0d8bbab2
Lester Pastrana <lesterpascua@gmail.com>
2020-08-07 23:13:50 +0200

    adding entity framework datetime utc converter

---
+++ SoftUnlimit.Data.EntityFramework/Utility/DateTimeUtcConverter.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SoftUnlimit.Data.EntityFramework.Utility
+{
+    /// <summary>
+    /// Set value kind UTC to DateTime
+    /// </summary>
+    public class DateTimeUtcConverter : ValueConverter<DateTime, DateTime>
+    {
+        private DateTimeUtcConverter(
+            Expression<Func<DateTime, DateTime>> convertToProviderExpression,
+            Expression<Func<DateTime, DateTime>> convertFromProviderExpression,
+            ConverterMappingHints mappingHints = null) : base(convertToProviderExpression, convertFromProviderExpression, mappingHints)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static DateTimeUtcConverter()
+        {
+            Instance = new DateTimeUtcConverter(
+                convertToProviderExpression: date => CodeToDb(date, null),
+                convertFromProviderExpression: date => DbToCode(date)
+            );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static DateTimeUtcConverter Instance { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="colum"></param>
+        /// <returns></returns>
+        public static DateTimeUtcConverter CreateNew(string colum) => new DateTimeUtcConverter(
+            convertToProviderExpression: date => CodeToDb(date, colum),
+            convertFromProviderExpression: date => DbToCode(date)
+        );
+
+
+        #region Private Methods 
+
+        private static DateTime DbToCode(DateTime date)
+        {
+            if (date.Kind == DateTimeKind.Unspecified)
+                DateTime.SpecifyKind(date, DateTimeKind.Utc);
+            return date.ToUniversalTime();
+        }
+        private static DateTime CodeToDb(DateTime date, string colum = null)
+        {
+            if (date.Kind != DateTimeKind.Utc)
+                throw new InvalidOperationException($"Column {colum ?? string.Empty} datetime only accepts UTC date-time values");
+            return date;
+        }
+
+        #endregion
+    }
+}
