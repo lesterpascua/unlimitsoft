4496d8748f9d43d10e5306a15a829ac5d17a7661
Lester Pastrana <lesterpascua@gmail.com>
2020-09-05 22:56:24 +0200

    adding IApiService interface

---
+++ SoftUnlimit.Web/Client/IApiService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUnlimit.Web.Client
+{
+    /// <summary>
+    /// Base interface for all api services.
+    /// </summary>
+    public interface IApiService
+    {
+    }
+}
