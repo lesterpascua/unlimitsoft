9ccb3701ed4197c678e360edb9b3bd9c7f45a114
Lester Pastrana <lesterpascua@gmail.com>
2020-09-14 01:12:32 +0200

    add certificate validation direct

---
+++ SoftUnlimit.Web.AspNet/Filter/Authentication/DefaultCertificateValidation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SoftUnlimit.Web.AspNet.Filter.Authentication
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class DefaultCertificateValidation : ICertificateValidation
+    {
+        private readonly X509Certificate2 _certificate;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="certificate"></param>
+        /// <param name="password"></param>
+        public DefaultCertificateValidation(string certificate, string password)
+        {
+            _certificate = Load(certificate, password);
+        }
+        /// <inheritdoc />
+        public bool Validate(X509Certificate2 certificate) => _certificate.Thumbprint == certificate.Thumbprint;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rawCertificate"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static X509Certificate2 Load(string rawCertificate, string password)
+        {
+            if (string.IsNullOrWhiteSpace(rawCertificate))
+                return null;
+
+            string cleanHeaderValue = Uri.UnescapeDataString(rawCertificate);
+            byte[] bytes = Convert.FromBase64String(cleanHeaderValue);
+            try
+            {
+                return new X509Certificate2(bytes, password);
+            } catch { }
+            return null;
+        }
+    }
+}
---
+++ SoftUnlimit.Web.AspNet/Filter/Authentication/ICertificateValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SoftUnlimit.Web.AspNet.Filter.Authentication
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ICertificateValidation
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="certificate"></param>
+        /// <returns></returns>
+        bool Validate(X509Certificate2 certificate);
+    }
+}
