5910fdd7a2bbc06318b3b1cad5225590e927ad2f
Lester Pastrana <lesterpascua@gmail.com>
2020-09-24 13:18:15 +0200

    fix bug in resolver IJsonHelper interface

--- SoftUnlimit.Web.AspNet/ErrorHandling/ToResponseExceptionHandlerOptions.cs
+++ SoftUnlimit.Web.AspNet/ErrorHandling/ToResponseExceptionHandlerOptions.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -52,25 +53,24 @@ namespace SoftUnlimit.Web.AspNet.ErrorHandling
         /// <returns></returns>
         public static async Task ExceptionHandlerInternal(HttpContext context, IWebHostEnvironment environment, IEnumerable<IExceptionHandler> handlers = null, ILogger<ToResponseExceptionHandlerOptions> logger = null)
         {
-            var jsonHelper = context.Features.Get<IJsonHelper>();
             var feature = context.Features.Get<IExceptionHandlerFeature>();
+            var jsonHelper = context.RequestServices.GetService<IJsonHelper>();
 
             logger?.LogError(feature.Error, $"User: {context.User.Identity.Name}, logged in from: {context.GetIpAddress()}");
             if (handlers != null)
                 foreach (var handler in handlers.Where(x => x.ShouldHandle(context)))
                     await handler.HandleAsync(context);
 
-            var reason = environment.IsDevelopment() ? jsonHelper.Serialize(feature.Error).ToString() : "Consult admin for more information.";
-            var response = new Response<IDictionary<string, string[]>> {
+            var reason = environment.IsDevelopment() ? feature.Error : new Exception("Consult admin for more information.");
+            var response = new Response<IDictionary<string, Exception>> {
                 IsSuccess = false,
                 Code = StatusCodes.Status500InternalServerError,
-                Body = new Dictionary<string, string[]> {
-                        { string.Empty, new string[] { reason } }
-                    },
+                Body = new Dictionary<string, Exception> {
+                    { string.Empty, feature.Error }
+                },
                 UIText = "Server Error"
             };
 
-            
             context.Response.StatusCode = response.Code;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(jsonHelper.Serialize(response).ToString());
