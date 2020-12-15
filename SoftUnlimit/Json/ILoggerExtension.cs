f8901e8d62c8c1f2e4cc09183dde79dbc6a7e594
Lester Pastrana <lesterpascua@gmail.com>
2020-12-15 13:56:25 -0500

    use newtonsoft

--- SoftUnlimit/Json/ILoggerExtension.cs
+++ SoftUnlimit/Json/ILoggerExtension.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Text.Json;
 
 namespace SoftUnlimit.Json
 {
@@ -24,36 +24,28 @@ namespace SoftUnlimit.Json
         /// <param name="logger"></param>
         /// <param name="data"></param>
         /// <param name="eventId"></param>
-        public static void LogTraceJson(this ILogger logger, object data, EventId? eventId = null) => logger.LogTrace(eventId ?? Empty, JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = Indented }));
+        public static void LogTraceJson(this ILogger logger, object data, EventId? eventId = null) => logger.LogTrace(eventId ?? Empty, JsonConvert.SerializeObject(data, Indented ? Formatting.Indented : Formatting.None));
         /// <summary>
         /// 
         /// </summary>
         /// <param name="logger"></param>
         /// <param name="data"></param>
         /// <param name="eventId"></param>
-        public static void LogDebugJson(this ILogger logger, object data, EventId? eventId = null) => logger.LogDebug(eventId ?? Empty, JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = Indented }));
+        public static void LogDebugJson(this ILogger logger, object data, EventId? eventId = null) => logger.LogDebug(eventId ?? Empty, JsonConvert.SerializeObject(data, Indented ? Formatting.Indented : Formatting.None));
         /// <summary>
         /// 
         /// </summary>
         /// <param name="logger"></param>
         /// <param name="data"></param>
         /// <param name="eventId"></param>
-        public static void LogInformationJson(this ILogger logger, object data, EventId? eventId = null) => logger.LogInformation(eventId ?? Empty, JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = Indented }));
+        public static void LogInformationJson(this ILogger logger, object data, EventId? eventId = null) => logger.LogInformation(eventId ?? Empty, JsonConvert.SerializeObject(data, Indented ? Formatting.Indented : Formatting.None));
         /// <summary>
         /// 
         /// </summary>
         /// <param name="logger"></param>
         /// <param name="data"></param>
         /// <param name="eventId"></param>
-        public static void LogWarningJson(this ILogger logger, object data, EventId? eventId = null) => logger.LogWarning(eventId ?? Empty, JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = Indented }));
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="logger"></param>
-        /// <param name="data"></param>
-        /// <param name="exception"></param>
-        /// <param name="eventId"></param>
-        public static void LogErrorJson(this ILogger logger, object data, Exception exception, EventId? eventId = null) => logger.LogError(eventId ?? Empty, exception, JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = Indented }));
+        public static void LogWarningJson(this ILogger logger, object data, EventId? eventId = null) => logger.LogWarning(eventId ?? Empty, JsonConvert.SerializeObject(data, Indented ? Formatting.Indented : Formatting.None));
         /// <summary>
         /// 
         /// </summary>
@@ -61,6 +53,14 @@ namespace SoftUnlimit.Json
         /// <param name="data"></param>
         /// <param name="exception"></param>
         /// <param name="eventId"></param>
-        public static void LogCriticalJson(this ILogger logger, object data, Exception exception, EventId? eventId = null) => logger.LogCritical(eventId ?? Empty, exception, JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = Indented }));
+        public static void LogErrorJson(this ILogger logger, object data, Exception exception, EventId? eventId = null) => logger.LogError(eventId ?? Empty, exception, JsonConvert.SerializeObject(data, Indented ? Formatting.Indented : Formatting.None));
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="data"></param>
+        /// <param name="exception"></param>
+        /// <param name="eventId"></param>
+        public static void LogCriticalJson(this ILogger logger, object data, Exception exception, EventId? eventId = null) => logger.LogCritical(eventId ?? Empty, exception, JsonConvert.SerializeObject(data, Indented ? Formatting.Indented : Formatting.None));
     }
 }
