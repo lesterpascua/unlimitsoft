2a51c8d0708e743c58cbb6a45be8aa5a258d4167
Lester Pastrana <lesterpascua@gmail.com>
2021-04-28 00:45:21 +0200

    update validator to stop on first failure

--- SoftUnlimit.CQRS/Command/Validation/CommandValidator.cs
+++ SoftUnlimit.CQRS/Command/Validation/CommandValidator.cs
@@ -12,5 +12,17 @@ namespace SoftUnlimit.CQRS.Command.Validation
     public sealed class CommandValidator<TCommand> : AbstractValidator<TCommand>
         where TCommand : ICommand
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public CommandValidator()
+        {
+            CascadeMode = DefaultCascadeMode;
+        }
+
+        /// <summary>
+        /// Default value used for this validator
+        /// </summary>
+        public static CascadeMode DefaultCascadeMode { get; set; } = CascadeMode.Stop;
     }
 }
