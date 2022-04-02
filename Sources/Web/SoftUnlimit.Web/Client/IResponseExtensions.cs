453d00304a0feaa06df3709d531439804a80ea8b
Lester Pastrana <lesterpascua@gmail.com>
2022-04-02 18:00:42 +0200

    get body error

--- Sources/Web/SoftUnlimit.Web/Client/IResponseExtensions.cs
+++ Sources/Web/SoftUnlimit.Web/Client/IResponseExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace SoftUnlimit.Web.Client
+﻿using System.Collections.Generic;
+
+namespace SoftUnlimit.Web.Client
 {
     /// <summary>
     /// 
@@ -11,5 +13,10 @@
         /// <typeparam name="TBody"></typeparam>
         /// <returns></returns>
         public static TBody GetBody<TBody>(this IResponse self) => (TBody)self.GetBody();
+        /// <summary>
+        /// Get body for error body, by default all error has the this type.
+        /// </summary>
+        /// <returns></returns>
+        public static IDictionary<string, string[]> GetErrorBody(this IResponse self) => (IDictionary<string, string[]>)self.GetBody();
     }
 }
