9c3e9b4cac6e4860d1e773d198a0704c960cb7c1
Lester Pastrana <lesterpascua@gmail.com>
2023-02-01 09:40:38 +0100

    feat: use a dictionary to avoid the problem with multiples cache keys

--- Sources/Web/UnlimitSoft.Web/Client/BaseApiService.cs
+++ Sources/Web/UnlimitSoft.Web/Client/BaseApiService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
 namespace UnlimitSoft.Web.Client;
@@ -12,10 +13,10 @@ public abstract class BaseApiService : IApiService, IDisposable
 {
     private readonly IApiClient _apiClient;
     private readonly ICache? _cache;
-    private readonly bool _ignorePrevCache;
+    private readonly ConcurrentDictionary<object, object?>? _prevCache;
     private readonly ILogger? _logger;
 
-    
+
     /// <summary>
     /// 
     /// </summary>
@@ -27,8 +28,9 @@ public abstract class BaseApiService : IApiService, IDisposable
     {
         _apiClient = apiClient;
         _cache = cache;
-        _ignorePrevCache = ignorePrevCache;
         _logger = logger;
+        if (!ignorePrevCache)
+            _prevCache = new ConcurrentDictionary<object, object?>();
     }
 
 
@@ -43,15 +45,11 @@ public abstract class BaseApiService : IApiService, IDisposable
     /// <summary>
     /// If false save the previous value in the cache to in case the service is not available return this value.
     /// </summary>
-    protected bool IgnorePrevCache => _ignorePrevCache;
+    protected bool IgnorePrevCache => _prevCache is not null;
     /// <summary>
     /// Logger used in the service
     /// </summary>
     protected ILogger? Logger => _logger;
-    /// <summary>
-    /// Old cache value.
-    /// </summary>
-    protected object? PrevCache { get; set; }
 
 
     /// <inheritdoc />
@@ -70,29 +68,29 @@ public abstract class BaseApiService : IApiService, IDisposable
     /// <param name="factory"></param>
     /// <param name="key"></param>
     /// <returns></returns>
-    protected async ValueTask<TResult> TryCacheFirst<TResult>(Func<object, Task<TResult>> factory, string? key = null)
+    protected async ValueTask<TResult> TryCacheFirst<TResult>(Func<object, Task<TResult>> factory, object? key = null)
     {
         if (_cache is null)
             throw new InvalidOperationException("Cache is not set");
 
         var cacheKey = key ?? typeof(TResult).FullName!;
-        if (_ignorePrevCache)
+        if (_prevCache is null)
             return await _cache.GetOrCreateAsync(cacheKey, factory);
 
         try
         {
-            return await _cache.GetOrCreateAsync(cacheKey, async entry =>
+            return await _cache.GetOrCreateAsync(cacheKey, async cacheKey =>
             {
-                var aux = await factory(entry);
-                PrevCache = aux;
-                return aux;
+                var value = await factory(cacheKey);
+                _prevCache[cacheKey] = value;
+                return value;
             });
         }
         catch (Exception ex)
         {
             _logger?.LogWarning(ex, "Error retrieving cache");
-            if (PrevCache is not null)
-                return (TResult)PrevCache;
+            if (_prevCache.TryGetValue(cacheKey, out var value) && value is not null)
+                return (TResult)value;
 
             throw;
         }
