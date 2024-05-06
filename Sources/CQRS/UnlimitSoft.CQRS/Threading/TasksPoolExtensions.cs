1a06e671c7ab1471c42ec056b01e36c26a50e3a1
Lester Pastrana <lester.pastrana@mcsystems.com>
2024-05-06 13:10:55 +0200

    feat: add predispatch function to allow execute operation before the command run

--- Sources/CQRS/UnlimitSoft.CQRS/Threading/TasksPoolExtensions.cs
+++ Sources/CQRS/UnlimitSoft.CQRS/Threading/TasksPoolExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 using UnlimitSoft.CQRS.Command;
 using UnlimitSoft.Threading;
 
@@ -15,18 +17,22 @@ public static class TasksPoolExtensions
     /// </summary>
     /// <param name="this"></param>
     /// <param name="name"></param>
+    /// <param name="preDispatch"></param>
     /// <param name="dispatcher"></param>
     /// <param name="command"></param>
     /// <param name="cleanMemory"></param>
     /// <param name="logger"></param>
     /// <returns></returns>
-    public static int Run(this TasksPool @this, string name, ICommandDispatcher dispatcher, ICommand command, bool cleanMemory, ILogger logger)
+    public static int Run(this TasksPool @this, string name, ICommandDispatcher dispatcher, ICommand command, Func<ValueTask>? preDispatch, bool cleanMemory, ILogger logger)
     {
         return @this.Run(
             name,
             async () =>
             {
                 logger.LogInformation("Start task {Name}", name);
+
+                if (preDispatch is not null)
+                    await preDispatch();
                 var result = await dispatcher.DispatchDynamicAsync(command);
 
                 logger.LogInformation("End task {Name} with result {Result}", name, result);
@@ -42,16 +48,20 @@ public static class TasksPoolExtensions
     /// <param name="name"></param>
     /// <param name="dispatcher"></param>
     /// <param name="command"></param>
+    /// <param name="preDispatch"></param>
     /// <param name="cleanMemory"></param>
     /// <param name="logger"></param>
     /// <returns></returns>
-    public static int Run<TResponse>(this TasksPool @this, string name, ICommandDispatcher dispatcher, ICommand<TResponse> command, bool cleanMemory, ILogger logger)
+    public static int Run<TResponse>(this TasksPool @this, string name, ICommandDispatcher dispatcher, ICommand<TResponse> command, Func<ValueTask>? preDispatch, bool cleanMemory, ILogger logger)
     {
         return @this.Run(
             name,
             async () =>
             {
                 logger.LogInformation("Start task {Name}", name);
+
+                if (preDispatch is not null)
+                    await preDispatch();
                 var result = await dispatcher.DispatchAsync(command);
 
                 logger.LogInformation("End task {Name} with result {Result}", name, result);
