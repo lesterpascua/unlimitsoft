c4be73ee545c9817a496d0e8201d76035617a4df
Lester Pastrana <lester.pastrana@mcsystems.com>
2024-07-27 15:22:46 +0200

    feat: expose fake lock handler in low level assembly to use in distribute features

---
+++ Sources/Core/UnlimitSoft/Distribute/LockHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnlimitSoft.Distribute;
+
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class LockHandler : ILockHandler
+{
+    private static ILockHandler? _none;
+
+    private LockHandler() { }
+
+    /// <inheritdoc />
+    public CancellationToken HandleLostToken => CancellationToken.None;
+
+    /// <inheritdoc />
+    public void Dispose() { }
+    /// <inheritdoc />
+    public ValueTask DisposeAsync() => default;
+
+    /// <summary>
+    /// Return a fake lock handler used to simulate a lock without any real lock
+    /// </summary>
+    public static ILockHandler None
+    {
+        get
+        {
+            if (_none is not null)
+                return _none;
+
+            Interlocked.CompareExchange(ref _none, null, new LockHandler());
+            return _none!;
+        }
+    }
+}
--- Sources/Web/UnlimitSoft.Web.AspNet.Testing/SysLockFake.cs
+++ Sources/Web/UnlimitSoft.Web.AspNet.Testing/SysLockFake.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using UnlimitSoft.Distribute;
 
-
 namespace UnlimitSoft.Web.AspNet.Testing;
 
 
@@ -19,25 +18,8 @@ public sealed class SysLockFake : ISysLock
     /// </summary>
     public static SysLockFake Instance => _instance;
 
-    /// <summary>
-    /// Return a fake lock handler used to simulate a lock without any real lock
-    /// </summary>
-    public static readonly ILockHandler None = new LockHandler();
-
     /// <inheritdoc />
-    public ValueTask<ILockHandler> AcquireAsync(string name, TimeSpan? timeOut = null, CancellationToken ct = default) => ValueTask.FromResult(None);
+    public ValueTask<ILockHandler> AcquireAsync(string name, TimeSpan? timeOut = null, CancellationToken ct = default) => ValueTask.FromResult(LockHandler.None);
     /// <inheritdoc />
-    public ValueTask<ILockHandler?> TryAcquireAsync(string name, TimeSpan timeOut = default, CancellationToken ct = default) => ValueTask.FromResult<ILockHandler?>(None);
-
-    #region Nested Classes
-    private sealed class LockHandler : ILockHandler
-    {
-        public CancellationToken HandleLostToken => CancellationToken.None;
-
-        /// <inheritdoc />
-        public void Dispose() { }
-        /// <inheritdoc />
-        public ValueTask DisposeAsync() => ValueTask.CompletedTask;
-    }
-    #endregion
+    public ValueTask<ILockHandler?> TryAcquireAsync(string name, TimeSpan timeOut = default, CancellationToken ct = default) => ValueTask.FromResult<ILockHandler?>(LockHandler.None);
 }
