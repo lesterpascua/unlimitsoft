172ef412ca3d36b0e9befd3ab0f6fb7e3c7114ca
Lester Pastrana <lester.pastrana@mcsystems.com>
2024-08-28 14:17:43 +0200

    feat: add base repository and raw SqlServer Statment generator

---
+++ Sources/Data/UnlimitSoft.Data/RawBaseRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data;
+using System.Threading;
+
+namespace UnlimitSoft.Data;
+
+
+/// <summary>
+/// Represents a base repository for raw data access.
+/// </summary>
+public abstract class BaseRawRepository : IDbConnectionFactory, IDisposable
+{
+    private bool _disposed = false;
+    private IDbConnection? _connection;
+
+    private readonly string _connString;
+
+    /// <summary>
+    /// Finalizes an instance of the <see cref="BaseRawRepository"/> class.
+    /// </summary>
+    ~BaseRawRepository()
+    {
+        Dispose(false);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BaseRawRepository"/> class with the specified connection string.
+    /// </summary>
+    /// <param name="connString">The connection string.</param>
+    public BaseRawRepository(string connString)
+    {
+        _connString = connString;
+        TimeOut = GetConnectionTimeOut(connString);
+    }
+
+    /// <inheritdoc />
+    public int TimeOut { get; }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <inheritdoc />
+    public IDbConnection GetDbConnection()
+    {
+        Interlocked.CompareExchange(ref _connection, CreateNewDbConnection(), null);
+        return _connection;
+    }
+
+    #region Protected Methods
+    /// <summary>
+    /// Gets the connection string.
+    /// </summary>
+    protected string ConnectionString => _connString;
+
+    /// <summary>
+    /// Releases the unmanaged resources used by the <see cref="BaseRawRepository"/> and optionally releases the managed resources.
+    /// </summary>
+    /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+            return;
+
+        if (disposing)
+            _connection?.Dispose();
+        _disposed = true;
+    }
+
+    /// <inheritdoc />
+    public abstract IDbConnection CreateNewDbConnection();
+    /// <summary>
+    /// Gets the connection timeout value based on the specified connection string.
+    /// </summary>
+    /// <param name="connString">The connection string.</param>
+    /// <returns>The connection timeout value.</returns>
+    protected abstract int GetConnectionTimeOut(string connString);
+    #endregion
+}
---
+++ Sources/Data/UnlimitSoft.Data/SqlServer/Statment.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using UnlimitSoft.Web.Model;
+
+namespace UnlimitSoft.Data.SqlServer;
+
+
+/// <summary>
+/// 
+/// </summary>
+public static class Statment
+{
+    /// <summary>
+    /// Gets the ORDER BY statement based on the specified order and default column.
+    /// </summary>
+    /// <param name="order">The list of column names and sort order.</param>
+    /// <param name="defaultColumn">The default column name.</param>
+    /// <param name="query"></param>
+    /// <returns>The ORDER BY statement.</returns>
+    public static void AppendOrderStatment(IReadOnlyList<ColumnName> order, string defaultColumn, StringBuilder query)
+    {
+        switch (order.Count)
+        {
+            case 0:
+                query.Append("ORDER BY [").Append(defaultColumn).Append(']');
+                break;
+            case 1:
+                query.Append("ORDER BY [").Append(order[0].Name).Append("] ").Append(order[0].Asc ? "ASC" : "DESC");
+                break;
+            default:
+                query.Append("ORDER BY");
+                foreach (var entry in order)
+                    query.Append(" [").Append(entry.Name).Append(']').Append(entry.Asc ? " ASC" : " DESC");
+                break;
+        }
+    }
+}
